52c507acd75bb00f948f55f196527a988ce9fff3
Megan Sorour <srrmeg001@myuct.ac.za>
2023-08-22 12:03:58 +0200

    First task done

--- Prac2/WorkPackage2/Prac2_student/Core/Src/main.c
+++ Prac2/WorkPackage2/Prac2_student/Core/Src/main.c
@@ -53,7 +53,7 @@ TIM_HandleTypeDef htim16;
 
 /* USER CODE BEGIN PV */
 // TODO: Define any input variables
-static uint8_t patterns[] = {};
+static uint8_t patterns[] = {10101010, 01010101, 11001100, 00110011, 11110000, 00001111};
 
 
 /* USER CODE END PV */
