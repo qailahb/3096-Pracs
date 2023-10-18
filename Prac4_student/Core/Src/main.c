512400ab88bb62c9024981e0278cb6012abdc49a
Qailah Bhamjee <qailahb@gmail.com>
2023-10-18 16:26:44 +0200

    Prac 4 code

--- Prac4_student/Core/Src/main.c
+++ Prac4_student/Core/Src/main.c
@@ -34,9 +34,9 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 // TODO: Add values for below variables
-#define NS        // Number of samples in LUT
-#define TIM2CLK   // STM Clock frequency
-#define F_SIGNAL  // Frequency of output analog signal
+#define NS 128       // Number of samples in LUT
+#define TIM2CLK 8000000  // STM Clock frequency
+#define F_SIGNAL 100 // Frequency of output analog signal
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -52,14 +52,39 @@ DMA_HandleTypeDef hdma_tim2_ch1;
 /* USER CODE BEGIN PV */
 // TODO: Add code for global variables, including LUTs
 
-uint32_t Sin_LUT[NS] = {};
+uint32_t Sin_LUT[NS] = {512, 541, 570, 598, 625, 652, 677, 702, 726, 748, 769, 789, 807, 825, 841, 856,
+		  869, 881, 892, 901, 909, 916, 922, 926, 930, 932, 933, 933, 932, 930, 927, 923,
+		  918, 913, 906, 898, 890, 881, 871, 860, 849, 837, 824, 811, 797, 783, 769, 755,
+		  740, 726, 711, 697, 683, 669, 656, 644, 631, 620, 609, 599, 590, 582, 574, 567,
+		  561, 556, 552, 549, 547, 546, 547, 548, 551, 555, 560, 566, 573, 581, 589, 598,
+		  608, 619, 630, 642, 655, 668, 682, 696, 710, 725, 740, 756, 771, 787, 803, 819,
+		  835, 851, 867, 883, 899, 914, 930, 945, 959, 974, 988, 1002, 1016, 1029, 1042,
+		  1055, 1067, 1079, 1090, 1101, 1112, 1122, 1131, 1140, 1149, 1157, 1164, 1170, 1176,
+		  1181, 1185, 1189, 1192, 1194, 1195, 1196, 1195, 1194, 1192, 1190, 1186, 1182, 1177,
+		  1172, 1166, 1159, 1151, 1143, 1134, 1125, 1115, 1104, 1093, 1081, 1069, 1056, 1043,
+		  1030, 1016, 1002, 989, 975, 961, 947, 934, 921, 909, 896, 885, 874, 864, 854, 846,
+		  837, 830, 823, 817, 811, 806, 802, 798, 795, 793, 792, 791, 792};
 
-uint32_t saw_LUT[NS] = {};
+uint32_t saw_LUT[NS] = {0, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104, 112, 120,
+		  128, 136, 144, 152, 160, 168, 176, 184, 192, 200, 208, 216, 224, 232, 240, 248,
+		  256, 264, 272, 280, 288, 296, 304, 312, 320, 328, 336, 344, 352, 360, 368, 376,
+		  384, 392, 400, 408, 416, 424, 432, 440, 448, 456, 464, 472, 480, 488, 496, 504,
+		  512, 520, 528, 536, 544, 552, 560, 568, 576, 584, 592, 600, 608, 616, 624, 632,
+		  640, 648, 656, 664, 672, 680, 688, 696, 704, 712, 720, 728, 736, 744, 752, 760,
+		  768, 776, 784, 792, 800, 808, 816, 824, 832, 840, 848, 856, 864, 872, 880, 888,
+		  896, 904, 912, 920, 928, 936, 944, 952, 960, 968, 976, 984, 992, 1000, 1008, 1016};
 
-uint32_t triangle_LUT[NS] = {};
+uint32_t triangle_LUT[NS] = {0, 16, 32, 48, 64, 80, 96, 112, 128, 144, 160, 176, 192, 208, 224, 240,
+		  256, 272, 288, 304, 320, 336, 352, 368, 384, 400, 416, 432, 448, 464, 480, 496,
+		  512, 528, 544, 560, 576, 592, 608, 624, 640, 656, 672, 688, 704, 720, 736, 752,
+		  768, 784, 800, 816, 832, 848, 864, 880, 896, 912, 928, 944, 960, 976, 992, 1008,
+		  1024, 1008, 992, 976, 960, 944, 928, 912, 896, 880, 864, 848, 832, 816, 800, 784,
+		  768, 752, 736, 720, 704, 688, 672, 656, 640, 624, 608, 592, 576, 560, 544, 528,
+		  512, 496, 480, 464, 448, 432, 416, 400, 384, 368, 352, 336, 320, 304, 288, 272,
+		  256, 240, 224, 208, 192, 176, 160, 144, 128, 112, 96, 80, 64, 48, 32, 16};
 
 // TODO: Equation to calculate TIM2_Ticks
-uint32_t TIM2_Ticks = 0; // How often to write new LUT value
+uint32_t TIM2_Ticks = (uint32_t) (TIM2CLK/(NS*F_SIGNAL)); // How often to write new LUT value
 uint32_t DestAddress = (uint32_t) &(TIM3->CCR3); // Write LUT TO TIM3->CCR3 to modify PWM duty cycle
 
 /* USER CODE END PV */
@@ -112,19 +137,21 @@ int main(void)
 
   /* USER CODE BEGIN 2 */
   // TODO: Start TIM3 in PWM mode on channel 3
-
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
 
   // TODO: Start TIM2 in Output Compare (OC) mode on channel 1.
-
+  HAL_TIM_OC_Start(&htim2, TIM_CHANNEL_1);
 
   // TODO: Start DMA in IT mode on TIM2->CH1; Source is LUT and Dest is TIM3->CCR3; start with Sine LUT
-
+  HAL_DMA_Start_IT(&hdma_tim2_ch1, (uint32_t)Sin_LUT, DestAddress, NS);
 
   // TODO: Write current waveform to LCD ("Sine")
   delay(3000);
+  lcd_command(CLEAR); //clear LCD
+  lcd_putstring("Sine");
 
   // TODO: Enable DMA (start transfer from LUT to CCR)
-
+  __HAL_TIM_ENABLE_DMA(&htim2, TIM_DMA_CC1);
 
   /* USER CODE END 2 */
 
@@ -346,11 +373,44 @@ static void MX_GPIO_Init(void)
 /* USER CODE END MX_GPIO_Init_2 */
 }
 
+uint32_t buttonPushed = 0;
+
 /* USER CODE BEGIN 4 */
 void EXTI0_1_IRQHandler(void)
 {
 	// TODO: Debounce using HAL_GetTick()
+			uint32_t tick = HAL_GetTick();					// Debouncing
 
+			if (tick - lastTick > debounceDelay) {			// Check time between last and present button press
+
+				// Toggles LED states
+				if (buttonPushed == 0) {
+								// increment waveform
+					buttonPushed = 1;
+				}
+				else if (buttonPushed == 1){
+								// increment waveform
+					buttonPushed = 2;
+				}
+				else if (buttonPushed == 2){
+								// increment waveform
+					buttonPushed = 3;
+				}
+				else if (buttonPushed == 3){
+								// increment waveform
+					buttonPushed = 0;
+				}
+
+				lastTick = tick;
+
+				return;
+//			}
+
+		__HAL_TIM_DISABLE_DMA(&htim2, TIM_DMA_CC1);
+		HAL_DMA_Abort_IT(&hdma_tim2_ch1);
+
+
+		HAL_GPIO_EXTI_IRQHandler(Button0_Pin); // Clear interrupt flags
 
 	// TODO: Disable DMA transfer and abort IT, then start DMA in IT mode with new LUT and re-enable transfer
 	// HINT: Consider using C's "switch" function to handle LUT changes
